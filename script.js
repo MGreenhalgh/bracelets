fb20d2da1744d42bca88b207eedec9b6d49e3e2b
MGreenhalgh <greenhalgh.mat@gmail.com>
2024-07-31 22:19:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => populatePage());
-
 async function fetchJSON() { //Load all the JSON data and return after it's all loaded
     var r = await fetch('songs.json'); var data = await r.json(); return data;
 }
-
 let songs
-
 async function populatePage() {
     let data = await fetchJSON(); //Wait for all the JSON data to load from this function before we do anything else
     if (data) console.log('JSON song data loaded');
-
     songs = JSON.parse(JSON.stringify(data.songs))
-
 }
+
+document.onkeyup = enter;
+function enter(e) { Check(); }
 
 const Check = () => {
 
@@ -37,34 +35,44 @@ const Check = () => {
         if (letterCount == wordLength) matches.push(word)
     }
 
-
     document.getElementById("resultsHolder").classList.remove("hidden")
 
     document.getElementById("resultsText").innerHTML = "You can make:"
-    if (matches.length == 0) document.getElementById("results").innerHTML = "Nothing 😭"
+    if (matches.length == 0) document.getElementById("results").innerHTML = "<div class='result'>Nothing 😭</div>"
     else {
         document.getElementById("results").innerHTML = ""
         for (let i = 0; i < matches.length; i++) {
-            document.getElementById("results").innerHTML += "<div class='result'>" + matches[i] + "</div>"
+            let result = document.createElement("div")
+            result.classList.add('result')
+            let removeButton = document.createElement("button")
+            removeButton.id = i
+            removeButton.classList.add("removeButton")
+            let resultText = document.createElement("div")
+            resultText.innerHTML += matches[i]
+            result.appendChild(removeButton)
+            result.appendChild(resultText)
+            document.getElementById("results").appendChild(result)
+
+            document.getElementById(i).addEventListener("click", (e) => { Remove(matches[i].split('(')[0].toLowerCase()) })
         }
     }
 
     if (document.getElementById("charInput").value == "") {
-        document.getElementById("resultsText").innerHTML = ""
-        document.getElementById("results").innerHTML = ""
+        document.getElementById("resultsHolder").classList.add("hidden")
     }
-
-    console.log(matches)
 }
 
-document.onkeyup = enter;
-function enter(e) { Check(); }
+const Remove = (str) => {
+    if (!confirm("Remove the letters " + str + "?")) return
 
-function changeViewMode() {
-    if (document.documentElement.getAttribute("data-theme") === "dark") {
-        document.documentElement.setAttribute("data-theme", "light");
+    let charString = document.getElementById("charInput").value
+    for (let i = 0; i < str.length; i++) {
+        const element = str[i];
+        if (charString.includes(str[i])) {
+            let letterPos = charString.indexOf(str[i])
+            charString = charString.slice(0, letterPos).concat(charString.slice(letterPos + 1))
+        }
     }
-    else if (document.documentElement.getAttribute("data-theme") === "light") {
-        document.documentElement.setAttribute("data-theme", "dark");
-    }
+    document.getElementById("charInput").value = charString
+    Check()
 }
